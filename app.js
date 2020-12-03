77658e56101619e00c62effdb1121e53b173d680
daanybarbosa <daanybsilva@gmail.com>
2020-12-03 00:42:14 -0300

    commit das ultimas modificações do projeto Multiroom chat

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./config/server');
 
-var server = app.listen(8080, function(){
+var server = app.listen(8080, function () {
     console.log('Servidor Online: localhost:8080');
 })
 
@@ -8,36 +8,38 @@ var io = require('socket.io').listen(server);
 
 app.set('io', io);
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('Usuario conectou');
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('Usuario desconectou');
     });
 
-    
-    socket.on('msgParaServidor', function(data){
-        
+
+    socket.on('msgParaServidor', function (data) {
+
         /*dialogo*/
         socket.emit(
             'msgParaCliente',
-            {apelido: data.apelido, mensagem: data.mensagem}
+            { apelido: data.apelido, mensagem: data.mensagem }
         );
 
         socket.broadcast.emit(
             'msgParaCliente',
-            {apelido: data.apelido, mensagem: data.mensagem}
+            { apelido: data.apelido, mensagem: data.mensagem }
         );
 
         /*participantes*/
-        socket.emit(
-            'participantesParaCliente',
-            {apelido: data.apelido}
-        );
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
 
-        socket.broadcast.emit(
-            'participantesParaCliente',
-            {apelido: data.apelido}
-        );
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+        }
     })
 });
